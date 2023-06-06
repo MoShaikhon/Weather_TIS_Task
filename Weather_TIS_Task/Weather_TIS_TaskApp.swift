802a30b7b69fad77eb93b8ca8bfeac0945071e50
Mohamed Shaikhon <mohamed.shaikhon94@gmail.com>
2023-06-06 16:31:39 +0300

    Initial Commit

---
+++ Weather_TIS_Task/Weather_TIS_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TIS_TaskApp.swift
+//  Weather_TIS_Task
+//
+//  Created by Mohamad Shaikhon on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TIS_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
